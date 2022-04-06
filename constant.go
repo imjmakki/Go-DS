4999c27c873a6a692b7394f7f2390567d0293271
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-06 04:15:06 +0300

    Data Structures with Golang

--- constant.go
+++ constant.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	//typed constant
 	const days int = 7
+	const (
+		//untyped constant
+		min1 = 500
+		min2
+		min3
+	)
+	fmt.Println(min1, min2, min3)
 }
