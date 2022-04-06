1115fdb648ebb525b2914a3856fefdd5f689c367
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-06 14:16:07 +0300

    Data Structures with Golang

--- convert.go
+++ convert.go
@@ -1,8 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	var x = 3
 	var y = 3.1
 
 	x = x * int(y)
+
+	fmt.Println(x)
+
+	s := string(99)
+	fmt.Println(s)
+
+	var myStr = fmt.Sprintf("%f", 44.2)
+	fmt.Println(myStr)
 }
