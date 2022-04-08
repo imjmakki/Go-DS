23ff5e5754750413b9acb302415c375eaa72c6f2
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 01:08:23 +0300

    Data Structures with Golang

--- simple.go
+++ simple.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -18,5 +19,13 @@ func main() {
 		fmt.Println("No error, i is:", i)
 	} else {
 		fmt.Println(err)
+	}
+
+	if args := os.Args; len(args) != 2 {
+		fmt.Println("One argument is required!")
+	} else if km, err := strconv.Atoi(args[1]); err != nil {
+		fmt.Println("The argument must be an integer! Error:", err)
+	} else {
+		fmt.Printf("%d km in miles is %v\n", km, float64(km)*1.609)
 	}
 }
