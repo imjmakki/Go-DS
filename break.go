55d1e53732f797e98ba1c46f284432d6bbec5e8e
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 01:31:30 +0300

    Data Structures with Golang

--- break.go
+++ break.go
@@ -7,7 +7,7 @@ func main() {
 
 	for i := 0; true; i++ {
 		if i%13 == 0 {
-			fmt.Printf("%d is divisible by 13 \n", i)
+			fmt.Printf("%d is divided by 13 \n", i)
 			count++
 		}
 		if count == 10 {
