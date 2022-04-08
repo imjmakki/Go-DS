d1e308203fd7586e9f430b9fe3d482ed0c87ca36
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 01:56:27 +0300

    Data Structures with Golang

--- switch.go
+++ switch.go
@@ -1,9 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	language := "Golang"
 
 	switch language {
-
+	case "Python":
+		fmt.Println("you are learning Python! You don't use curly braces but indentation!!")
 	}
 }
