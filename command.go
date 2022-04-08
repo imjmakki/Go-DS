622f72d2b18bce11c73695dc88aa2c5987e22806
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 00:47:28 +0300

    Data Structures with Golang

--- command.go
+++ command.go
@@ -10,5 +10,5 @@ func main() {
 	fmt.Println("Path: ", os.Args[0])
 	fmt.Println("1st arguments: ", os.Args[1])
 	fmt.Println("2nd arguments: ", os.Args[2])
-	fmt.Println("No. if items inside os.Args: ", len(os.Args))
+	fmt.Println("No. of items inside os.Args: ", len(os.Args))
 }
