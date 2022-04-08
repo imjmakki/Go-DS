9332e42bcf725905d1e4d89cabd3a9b081eeda5f
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 01:40:37 +0300

    Data Structures with Golang

--- label.go
+++ label.go
@@ -2,11 +2,11 @@ package main
 
 func main() {
 	people := [5]string{"Helen", "Mark", "Brenda", "Antonio", "Micheal"}
-	frinds := [2]string{"Mark", "Marry"}
+	friends := [2]string{"Mark", "Marry"}
 
 outer:
 	for index, name := range people {
-		for _, frinds := range frinds {
+		for _, friends := range friends {
 
 		}
 	}
