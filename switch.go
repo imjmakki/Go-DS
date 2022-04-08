d381a6532e7cd75b47f461cfd580a1ff9d64c447
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 01:59:21 +0300

    Data Structures with Golang

--- switch.go
+++ switch.go
@@ -10,7 +10,7 @@ func main() {
 		fmt.Println("you are learning Python! You don't use curly braces but indentation!!")
 		break
 	case "Go", "Golang":
-		fmt.Println("Great, go for Go :)")
+		fmt.Println("Great, go for Go :) You are using curley braces")
 		break
 	}
 }
