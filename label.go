ebdd1f17b22c74c7e985590c33e018029aa7b9aa
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-09 01:42:32 +0300

    Data Structures with Golang

--- label.go
+++ label.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	people := [5]string{"Helen", "Mark", "Brenda", "Antonio", "Micheal"}
 	friends := [2]string{"Mark", "Marry"}
@@ -8,7 +10,7 @@ outer:
 	for index, name := range people {
 		for _, friend := range friends {
 			if name == friend {
-
+				fmt.Printf("Found a friend %q at index %d\n", friend, index)
 			}
 		}
 	}
