7b317045e09a1f700e2e40cb743bf062c33be2c6
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-10 19:52:11 +0300

    Data Structures with Golang

--- byte.go
+++ byte.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	name := "Codruta"
 	fmt.Println(len(name))
+
+	n := utf8.RuneCountInString(name)
+	fmt.Println(n)
 }
