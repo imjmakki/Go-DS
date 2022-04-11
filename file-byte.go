ca4c5464bda9073765e56b447987c23315b94c0a
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-11 17:46:43 +0300

    Data Structures with Golang

--- file-byte.go
+++ file-byte.go
@@ -8,10 +8,11 @@ import (
 func main() {
 	file, err := os.OpenFile(
 		"b.txt",
-		os.O_WRONLY|os.O_TRUNC|os.O_CREATE
-			0644,
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE,
+		0644,
 	)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 }
