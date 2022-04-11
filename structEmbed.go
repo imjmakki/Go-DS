7a3379f1d73c2950f9f88bf1cb1fc6e737e9eb03
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-11 23:42:04 +0300

    Data Structures with Golang

--- structEmbed.go
+++ structEmbed.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
-
+	type Contact struct {
+		email, address string
+		phone          int
+	}
+	type Employee struct {
+		name        string
+		salary      int
+		contactInfo Contact
+	}
 }
