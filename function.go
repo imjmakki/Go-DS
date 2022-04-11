19a5e846e1ec9969107fce6fa758b67d61c1c1a2
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-12 01:52:59 +0300

    Data Structures with Golang

--- function.go
+++ function.go
@@ -18,11 +18,13 @@ func f3(a, b, c int, d, e float64, s string) {
 }
 
 func f4(a float64) float64 {
-	return math.Pow()
+	return math.Pow(a, a)
 }
 
 func main() {
 	f1()
 	f2(5, 7)
 	f3(4, 5, 6, 4.4, 7.8, "Golang")
+	p := f4(5.1)
+	fmt.Println(p)
 }
