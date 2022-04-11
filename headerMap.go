f19d8010c6efbfabeec2932475bbe07488e0a22c
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-11 12:00:53 +0300

    Data Structures with Golang

--- headerMap.go
+++ headerMap.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	friends := map[string]int{"Dan": 40, "Maria": 25}
+	neighbors := friends
+	fmt.Println(neighbors)
+
+	friends["Dan"] = 50
+	fmt.Println(neighbors)
 }
