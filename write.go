1630996f1fcd20d063391b2762ab37e2d7f60b58
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-11 18:08:27 +0300

    Data Structures with Golang

--- write.go
+++ write.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	file, err := os.OpenFile("myFile.txt", os.O_TRUNC|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("c.txt", os.O_TRUNC|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
