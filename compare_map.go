b0d5ea5f760c31037f6518c99e60080b6abd75ca
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-11 11:53:20 +0300

    Data Structures with Golang

--- compare_map.go
+++ compare_map.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := map[string]string{"X": "Y"}
+	b := map[string]string{"A": "Y"}
+
+	s1 := fmt.Sprintf("%s", a)
+	s2 := fmt.Sprintf("%s", b)
+
+	fmt.Println(s1, s2)
 }
