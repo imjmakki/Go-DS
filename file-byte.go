f5a3e655d4e53a59d7758c83446eeba17e787faa
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-11 17:43:05 +0300

    Data Structures with Golang

--- file-byte.go
+++ file-byte.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+)
 
+func main() {
+	file, err := os.OpenFile(
+		"b.txt",
+		os.O_WRONLY|os.O_TRUNC|os.O_CREATE
+			0644,
+	)
+	if err != nil{
+		log.Fatal(err)
+	}
 }
