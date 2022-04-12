e5929da592aa3e3da59d413ee6dd76af4d6d146a
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-12 11:32:53 +0300

    Data Structures with Golang

--- pass_point.go
+++ pass_point.go
@@ -11,5 +11,5 @@ func changeValues(quantity int, price float64, name string, sold bool) {
 
 func main() {
 	quantity, price, name, sold := 5, 300.4, "Laptop", true
-	fmt.Println("", quantity, price, name, sold)
+	fmt.Println("Before calling changeValue():", quantity, price, name, sold)
 }
