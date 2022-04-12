5f2eee0d95ce450fa2851a537996b3c9f6bf7122
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-12 11:22:13 +0300

    Data Structures with Golang

--- pointer_pass.go
+++ pointer_pass.go
@@ -1,10 +1,19 @@
 package main
 
-func change(a *int) *float64 {
+import "fmt"
 
+func change(a *int) *float64 {
+	*a = 100
+
+	b := 5.5
+	return &b
 }
 
 func main() {
 	x := 8
 	k := &x
+
+	fmt.Println("value of x before calling change():", x)
+	change(k)
+	fmt.Println("value of x after calling change():", k)
 }
