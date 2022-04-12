99735016f26f7aeb2738078dceaa722d3b898b81
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-12 11:30:25 +0300

    Data Structures with Golang

--- pass_point.go
+++ pass_point.go
@@ -1,5 +1,12 @@
 package main
 
+func changeValues(quantity int, price float64, name string, sold bool) {
+	quantity = 3
+	price = 500.4
+	name = "Mobile Phone"
+	sold = false
+}
+
 func main() {
 
 }
