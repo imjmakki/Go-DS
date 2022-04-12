eff07e5860758986e4eebff9641ee3456243a419
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-12 11:06:56 +0300

    Data Structures with Golang

--- point_compare.go
+++ point_compare.go
@@ -8,4 +8,7 @@ func main() {
 	pp1 := p1
 	fmt.Printf("Value of p1: %v, address of p1: %v\n", p1, &p1)
 	fmt.Printf("Value of pp1: %v, address of pp1: %v\n", pp1, &pp1)
+
+	fmt.Printf("*p is %v\n", *p1)
+	fmt.Printf("*pp1 is %v\n", *pp1)
 }
