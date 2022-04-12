bd003b1f41054d4f62be663122ab1372d41b96d9
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-12 11:37:55 +0300

    Data Structures with Golang

--- pass_point.go
+++ pass_point.go
@@ -10,6 +10,10 @@ func changeValues(quantity int, price float64, name string, sold bool) {
 }
 
 func changeValuesByPointer(quantity *int, price *float64, name *string, sold *bool) {
+	*quantity = 3
+	*price = 500.4
+	*name = "Mobile Phone"
+	*sold = false
 }
 
 func main() {
