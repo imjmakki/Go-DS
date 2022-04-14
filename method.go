f6d22a100ef560cfce63bedde168d0e00e34414d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-15 02:15:22 +0300

    Data Structures with Golang

--- method.go
+++ method.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	const day = 24 * time.Hour
 	fmt.Printf("%T\n", day)
+
+	seconds := day.Seconds()
+	fmt.Printf("%T\n", seconds)
+	fmt.Printf("Seconds in a day: %v\n", seconds)
 }
