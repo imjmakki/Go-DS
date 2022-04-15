d450af5e68e5b6c563416657fbc668d6246d00b7
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-16 01:30:51 +0300

    Data Structures with Golang

--- gorout.go
+++ gorout.go
@@ -9,4 +9,6 @@ func main() {
 	fmt.Println("main execution started")
 	fmt.Println("No. of CPU's:", runtime.NumCPU())
 	fmt.Println("No. of Goroutines:", runtime.NumGoroutine())
+	fmt.Println("OS:", runtime.GOOS)
+	fmt.Println("Arch:", runtime.GOARCH)
 }
