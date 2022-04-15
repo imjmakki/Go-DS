4a864483ad240389fe8571a4bda03a84ae2913c6
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-16 01:37:29 +0300

    Data Structures with Golang

--- gorout.go
+++ gorout.go
@@ -5,6 +5,12 @@ import (
 	"runtime"
 )
 
+func l1() {
+	fmt.Println("l1 (goroutine) execution started")
+
+	fmt.Println("l1 execution finished")
+}
+
 func main() {
 	fmt.Println("main execution started")
 	fmt.Println("No. of CPU's:", runtime.NumCPU())
