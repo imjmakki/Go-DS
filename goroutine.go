3c3e0042472a83270f0bc43d37c1f2983bc4410d
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-16 01:21:23 +0300

    Data Structures with Golang

---
+++ goroutine.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doSomething() {
+	fmt.Println("This function says: \"Hello\"")
+}
+
+func main() {
+	doSomething()
+	go doSomething()
+	time.Sleep(time.Second * 2)
+}
