6f916ccf0ad9f7ab58e0af484b51f8da4fcf75a7
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-17 14:34:45 +0300

    go bootcamp

--- dataRace.go
+++ dataRace.go
@@ -12,16 +12,21 @@ func main() {
 	wg.Add(gr * 2)
 
 	var n int = 0
+	var m sync.Mutex
 
 	for i := 0; i < gr; i++ {
 		go func() {
 			time.Sleep(time.Second / 2)
+			m.Lock()
 			n++
+			m.Unlock()
 			wg.Done()
 		}()
 		go func() {
 			time.Sleep(time.Second / 2)
+			m.Lock()
 			n--
+			m.Unlock()
 			wg.Done()
 		}()
 	}
