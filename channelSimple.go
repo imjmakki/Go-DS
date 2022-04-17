64e50aa564781d16c7977b3a28c4f9b47e44c9c5
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-17 15:14:35 +0300

    go bootcamp

--- channelSimple.go
+++ channelSimple.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+func f1(n int, ch chan int) {
+	ch <- n
+}
+
 func main() {
 	c := make(chan int)
 	c1 := make(<-chan string)
 	c2 := make(chan<- string)
 
 	fmt.Println("%T", "%T", "%T", c, c1, c2)
+	go f1(10, c)
 }
