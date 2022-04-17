3a48bede7f5d06010d6cf94d720f91d069d51cb6
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-17 12:23:13 +0300

    go bootcamp

--- dataRace.go
+++ dataRace.go
@@ -1,9 +1,20 @@
 package main
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 func main() {
 	const gr = 100
 	var wg sync.WaitGroup
 	wg.Add(gr * 2)
+
+	var n int = 0
+
+	for i := 0; i < gr; i++ {
+		go func() {
+			time.Sleep(time.Second / 2)
+		}
+	}
 }
