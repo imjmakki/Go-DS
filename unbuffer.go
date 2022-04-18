48bce9ca1c8f4e2c42bbb28802f19b9c77dcba7f
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-04-18 10:53:17 +0300

    go bootcamp

--- unbuffer.go
+++ unbuffer.go
@@ -1,6 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
 	c1 := make(chan int)
 	c2 := make(chan int, 3)
+	go func(c chan int) {
+		fmt.Println("func goroutine starts sending data into the channel")
+		fmt.Println("func goroutine after sending data into the channel")
+	}()
 }
